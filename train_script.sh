4d05c0c9a5b97f7306b7573c23dd334586b40097
Hoanh Le <hoanh@scandit.com>
2021-04-03 02:30:52 +0300

    Fix train script

--- train_script.sh
+++ train_script.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+#SBATCH --time=00:30:00
+#SBATCH --job-name=train_pointnet
+#SBATCH --mem-per-cpu=1g
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --output=log_%j.txt
+
 module load cuda
 
 python train.py --data modelnet40 --model pointnet --feature_transform 0
